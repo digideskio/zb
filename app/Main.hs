9ed95801788d8204371f479df81661e50b0f2882
Yuki Izumi <yuki@kivikakk.ee>
2016-01-31 20:26:43 +1100

    types etc

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
+import qualified Data.Map.Strict as Map
 import Lib
+
+env :: [(String, Type, Value)]
+env = [
+        ("+",
+         TFunction [] (Just TInteger) TInteger,
+         VBuiltin (\args -> VInteger $ sum (map (\v -> case v of VInteger n -> n
+                                                                 _ -> error "uh oh") args)))
+      ]
 
 main :: IO ()
 main = do
-        sexp <- fmap parse getLine
-        print sexp
-        let aexp = compile sexp
+        ast <- fmap parse getLine
+        print ast
+        let le = Map.fromList (map (\(s,t,_) -> (s,t)) env)
+        let aexp = compile le ast
         print aexp
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,46 +3,72 @@
 module Lib
     ( parse
     , compile
-    , Sexp
-    , Aexp
+    , AST
+    , Type(..)
+    , TAST
+    , Value(..)
     ) where
 
 import qualified Data.Attoparsec.Text as A
 import qualified Data.Attoparsec.Combinator as AC
+import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import Control.Applicative ((<*>), (*>), (<$>), (<|>), pure)
 import Data.Attoparsec.Text (Parser)
+import Data.Map.Strict (Map)
 import Data.Text (Text)
 import Text.Read (readMaybe)
 
-data Sexp = SAtom String
-          | SList [Sexp]
-          deriving (Show)
+--
+-- parsing
+--
 
-parse :: String -> Sexp
-parse str = case A.parseOnly sexp (T.pack str) of
+data AST = SAtom String
+         | SInteger Integer
+         | SList [AST]
+         deriving (Show)
+
+parse :: String -> AST
+parse str = case A.parseOnly ast (T.pack str) of
                  Left e -> error e
                  Right r -> r
 
-sexp :: Parser Sexp
-sexp = atom <|> list
-       where atom = SAtom . T.unpack <$> A.takeWhile1 (A.inClass "a-zA-Z0-9")
-             list = SList <$> (A.string "(" *> AC.manyTill (A.skipSpace *> sexp <* A.skipSpace) (A.string ")"))
+ast :: Parser AST
+ast = integer <|> atom <|> list
+      where integer = SInteger . read . T.unpack <$> A.takeWhile1 (A.inClass "0-9")
+            -- broken: "1a" will read as (SInteger 1) (SAtom "a").
+            -- should maybe error or something instead.
+            -- should read until " \t\r\n)" or EOF. anything else is a mistake
+            atom = SAtom . T.unpack <$> A.takeWhile1 (A.inClass "-a-zA-Z0-9+*/_")
+            list = SList <$> (A.string "(" *> AC.manyTill (A.skipSpace *> ast <* A.skipSpace) (A.string ")"))
 
-data Lit = LInt Integer
-         deriving (Show)
+--
+-- compiling
+--
 
-data Aexp = ALiteral Lit
-          | AVariable String
-          | AFuncall Aexp [Aexp]
+data Type = TInteger
+          | TFunction [Type] (Maybe Type) Type  -- args, *args, return
           deriving (Show)
 
-compile :: Sexp -> Aexp
-compile s = case s of
-                 SAtom str -> case readMaybe str of
-                                   Just n -> ALiteral (LInt n)
-                                   Nothing -> AVariable str
+data Literal = LInteger Integer
+             deriving (Show)
 
-                 SList [] -> error "empty cell"
-                 SList sexps -> let (h:t) = map compile sexps in
-                                AFuncall h t
+data TAST = ALiteral Literal
+          | AVariable String Type
+          | AFuncall TAST [TAST]
+          deriving (Show)
+
+data Value = VInteger Integer
+           | VBuiltin ([Value] -> Value)
+
+type LexicalEnv = Map String Type
+
+compile :: LexicalEnv -> AST -> TAST
+compile le s = case s of
+                    SAtom str -> case Map.lookup str le of
+                                      Just typ -> AVariable str typ
+                                      Nothing -> error ("unbound variable " ++ str)
+                    SInteger n -> ALiteral (LInteger n)
+                    SList sexps -> case map (compile le) sexps of
+                                        [] -> error "empty cell"
+                                        h:t -> AFuncall h t
